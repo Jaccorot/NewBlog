e5118b4f9e09baeb006fe026549bc9f016f4bad9
Jaccorot <caozijun007@163.com>
2014-03-26 00:41:08 +0800

    follow some codes

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,34 @@
 from django.db import models
+from django.db import models
 
 
-class Poll(models.Model):
-	question = models.CharField(max_length = 200)
-	pub_date = models.DateTimeField('date published')
+class Tag(models.Model):
+    """docstring for Tags"""
+    tag_name = models.CharField(max_length=20, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.tag_name
 
 
-class Choice(models.Model):
-	poll = models.ForeignKey(Poll)
-	choice_text = models.CharField(max_length = 200)
-	votes = models.IntegerField(default = 0)
+class Author(models.Model):
+    """docstring for Author"""
+    name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True)
+    website = models.URLField(blank=True)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+
+class Blog(models.Model):
+    """docstring for Blogs"""
+    caption = models.CharField(max_length=50)
+    author = models.ForeignKey(Author)
+    tags = models.ManyToManyField(Tag, blank=True)
+    content = models.TextField()
+    publish_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u'%s %s %s' % (self.caption, self.author, self.publish_time)
