7d10d1cf31a324e529e65602cca9eeb35660a22a
Jaccorot <caozijun007@163.com>
2014-03-26 00:14:55 +0800

    fill something

--- NewBlog/settings.py
+++ NewBlog/settings.py
@@ -65,9 +65,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
