0a1c5cb34a0ac5ed79886abf3e9be018655dfd30
Jaccorot <caozijun007@163.com>
2014-04-22 21:17:39 +0800

    add the first two lines

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
